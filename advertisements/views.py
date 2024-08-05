a1126c28f96cf099eafeae8e481e5d585f8e9225
Rustam Sharipov <r.z.sharipov@gmail.com>
2024-08-05 20:36:58 +0500

    $

--- advertisements/views.py
+++ advertisements/views.py
@@ -1,17 +1,45 @@
+from rest_framework import permissions, viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
-from .models import Advertisement
-from .serializers import AdvertisementSerializer
-from .filters import AdvertisementFilter
+from rest_framework.exceptions import PermissionDenied
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
 
-class AdvertisementViewSet(ModelViewSet):
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Только автор объявления может изменить или удалить его
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.creator == request.user
+
+
+class AdvertisementViewSet(viewsets.ModelViewSet):
     """ViewSet для объявлений."""
+
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
-    filterset_class = AdvertisementFilter
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        """Создание объекта."""
+        serializer.save(creator=self.request.user)
+
+    def update(self, request, *args, **kwargs):
+        # Проверка что пользователь автор объявления
+        advertisement = self.get_object()
+        if advertisement.creator != request.user:
+            raise PermissionDenied('You do not have permission to modify this advertisement.')
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        # Проверка что пользователь автор объявления
+        advertisement = self.get_object()
+        if advertisement.creator != request.user:
+            raise PermissionDenied('You do not have permission to delete this advertisement.')
+        return super().destroy(request, *args, **kwargs)
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+        if self.action in ["create", "update", "partial_update", "destroy"]:
             return [IsAuthenticated()]
-        return []
+        return [permission() for permission in self.permission_classes]
--- api_with_restrictions/settings.py
+++ api_with_restrictions/settings.py
@@ -75,9 +75,14 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ]
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/hour',
+        'user': '1000/day'
+    }
 }
 
 WSGI_APPLICATION = 'api_with_restrictions.wsgi.application'
